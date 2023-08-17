b939d2ae2c25e2bd828594a597f06035bfcebc97
San Gillis <gillis.san@gmail.com>
2023-08-17 21:50:50 +0200

    Fix linter error

--- tests/kyu6_split_strings_test.py
+++ tests/kyu6_split_strings_test.py
@@ -3,4 +3,4 @@ from kyu6_split_strings import split_strings
 
 def test_split_strings():
     assert ['ab', 'c_'] == split_strings('abc')
-    assert ['ab', 'cd', 'ef'] == split_strings('abcdef') 
+    assert ['ab', 'cd', 'ef'] == split_strings('abcdef')
